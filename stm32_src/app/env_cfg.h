d6a16952bc96507142e38979a32e825d3fcca76d
panshengpeng <1441175260@qq.com>
2019-05-27 17:56:24 +0800

    add env_cfg

---
+++ stm32_src/app/env_cfg.h
@@ -0,0 +1,33 @@
+/*
+ * env_cfg.h
+ *
+ *  Created on: Mar 19, 2018
+ *      Author: chenzy
+ */
+
+#ifndef PARTS_FAULT_PARTITION_ENV_CFG_H_
+#define PARTS_FAULT_PARTITION_ENV_CFG_H_
+
+#include <stdint.h>
+
+
+void cfg_set_device_id(uint16_t id);
+uint16_t cfg_get_device_id(void);
+
+void cfg_set_device_version(char *version);
+char* cfg_get_device_version(void);
+
+void cfg_set_device_data_interval(uint32_t data_interval);
+uint32_t cfg_get_device_data_interval(void);
+
+void cfg_set_device_channel_threshold(uint8_t channel, uint32_t threshold);
+uint32_t cfg_get_device_channel_threshold(uint8_t channel);
+
+
+void load_device_cfg(void);
+void update_device_cfg(void);
+void display_device_cfg(void);
+
+
+
+#endif /* PARTS_FAULT_PARTITION_ENV_CFG_H_ */
---
+++ stm32_src/app/type_alias.h
@@ -0,0 +1,40 @@
+#ifndef TYPE_ALIAS_H_
+#define TYPE_ALIAS_H_
+
+#define MAX_CHANNEL 6
+#define MAX_FPGA_DATA_LEN (20480 + 4096) //24k
+#define MAX_PARTIAL_DISCHARGE_CURVE_DATA_LEN (102400) //100K
+
+//#define MASTER_ADDR 0X0001
+#define BASE_CHANNEL 0X0010
+
+#ifndef boolean
+typedef enum{
+   SD_TRUE = 0,
+   SD_FALSE = 1
+}boolean;
+#endif
+
+typedef enum {
+    DEVICEOK,
+    DEVTIMEERR,
+    GPSINFOERR,
+    ZGBINITERR,
+    FPGAINITERR,
+    SAMPLEERR,
+    TRANSFERERR,
+    MD5ERR,
+    FILECHAOSERR,
+    DEVBUSYERR,
+	DEVNODATA,
+	DEVNOSUP,
+} dev_status_t;
+
+typedef enum _FileType{
+    FPGA,
+    ARM,
+    FSMC_FPGA,
+    SPI_FPGA
+}FileType;
+
+#endif

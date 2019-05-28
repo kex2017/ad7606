d9a16dfd7a51a2d9ef879d644275552b1162a14c
panshengpeng <1441175260@qq.com>
2019-05-28 09:37:53 +0800

    add some encode

--- stm32_src/app/frame_encode.h
+++ stm32_src/app/frame_encode.h
@@ -5,4 +5,14 @@
 
 uint16_t frame_time_ctrl_data_encode(uint8_t *data, uint8_t errorcode, uint32_t timestamp);
 uint16_t frame_get_running_state_encode(uint8_t *data, uint8_t errorcode, uint32_t temp, uint32_t humidity, uint32_t voltage, uint8_t work , uint32_t timestamp );
+
+/*周期数据编码*/
+uint16_t current_cycle_data_encode(uint8_t *data, uint8_t errorcode, float ch1_current, float ch2_current, uint32_t timestamp);
+uint16_t high_current_cycle_data_encode(uint8_t *data, uint8_t errorcode, float ch1_current, float ch2_current, uint32_t timestamp);
+
+/*突变数据编码*/
+uint16_t high_current_mutation_data_encode(uint8_t * data,  uint8_t errorcode, uint32_t timestamp, uint8_t channel, uint16_t pkg_sum, uint16_t pkg_index, uint8_t * cur_data, uint16_t len );
+uint16_t current_mutation_data_encode(uint8_t * data,  uint8_t errorcode, uint32_t timestamp, uint8_t channel, uint16_t pkg_sum, uint16_t pkg_index, uint8_t * cur_data, uint16_t len );
+
+
 #endif /* FRAME_ENCODE_H_ */
